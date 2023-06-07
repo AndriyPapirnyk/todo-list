cdadf6e9e6eb8374013162eea3f81485c8470ec1
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-06-07 23:59:27 +0300

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function () {
-    return gulp.src('./src//!(_)*.scss') 
+    return gulp.src('./src/**/!(_)*.scss') 
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./src'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./src//!(_)*.scss', gulp.series('sass'));
+    gulp.watch('./src/**/!(_)*.scss', gulp.series('sass'));
 });
